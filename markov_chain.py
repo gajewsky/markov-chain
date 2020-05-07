e73f4130b9718404d9c97a7fd94d4b1d9e7bfe19
Marcin Gajewski <code@gajewsky.it>
2020-05-07 18:11:59 +0200

    Add words generator

--- markov_chain.py
+++ markov_chain.py
@@ -1,7 +1,9 @@
 import fire
 from markov_chain.text import Text
+from markov_chain.words_generator import WordsGenerator
 
 LOREM = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text Lorem since the 1500s, when an Lorem printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing"
+words = Text(LOREM).word_stats()
 
 if __name__ == '__main__':
-  fire.Fire(Text(LOREM).word_stats())
+  fire.Fire(WordsGenerator(words).call)
